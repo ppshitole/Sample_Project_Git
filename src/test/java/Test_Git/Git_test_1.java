eccce4ca4407c7d1b7e5b5421aea910e54e3358f
PANKAJ SHITOLE <95542661+ppshitole@users.noreply.github.com>
2024-06-10 23:49:54 +0530

    changes done in test1 and test2

--- src/test/java/Test_Git/Git_test_1.java
+++ src/test/java/Test_Git/Git_test_1.java
@@ -1,5 +1,9 @@
 package Test_Git;
 
-public class Git_test_1 {
-
+public class Git_test_1 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("This is Test_1");
+	}
 }
--- src/test/java/Test_Git/Git_test_2.java
+++ src/test/java/Test_Git/Git_test_2.java
@@ -1,5 +1,9 @@
 package Test_Git;
 
-public class Git_test_2 {
-
+public class Git_test_2 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("This is Test_2");
+	}
 }
